2af6054bd54bd0468af87bccc64d0e8f8ae3dfdd
Luana NOIRA <a47523@alunos.ipb.pt>
2022-05-05 17:39:25 +0100

    8.Update your GameEnding Script to Play Audio

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -9,12 +9,16 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
 
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
+    bool m_HasAudioPlayed;
+
 
     void OnTriggerEnter(Collider other)
     {
